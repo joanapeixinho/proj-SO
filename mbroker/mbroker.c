14271169902a4a2feeafdd927ec610d02e829c29
Peixinho <joanapeixinho0@gmail.com>
2022-12-27 14:27:51 +0000

    mbroker reads commands

--- mbroker/mbroker.c
+++ mbroker/mbroker.c
@@ -1,9 +1,18 @@
 #include "logging.h"
 
+
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    fprintf(stderr, "usage: mbroker <pipename>\n");
-    WARN("unimplemented"); // TODO: implement
+
+    if (argc != 3) {
+        fprintf(stderr, "Usage: mbroker <pipename> <max_sessions>\n");
+        return -1;
+    }    
+
+    
+    char* pipename = argv[1];
+    int max_sessions = atoi(argv[2]);
+
+    printf("mbroker %s %d\n", pipename, max_sessions);
+
     return -1;
 }
