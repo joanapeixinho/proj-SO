2112100b9f62f134fb505636c2354f7a795b27bb
Sepremo <miguelfernandes233@gmail.com>
2022-12-30 12:48:30 +0000

    Pub continuously reads stdin

--- publisher/pub.c
+++ publisher/pub.c
@@ -1,4 +1,6 @@
 #include "logging.h"
+#include <stdbool.h>
+#include <common/common.h>
 
 int main(int argc, char **argv) {
     if (argc != 3) {
@@ -10,8 +12,26 @@ int main(int argc, char **argv) {
     char* pipe_name = argv[2];
     char* box_name = argv[3];
 
-    // TODO: implement
+    //TODO: Try to register the publisher
+
+
+
+    //Read from stdin until user presses ctrl+d
+    char* message[MESSAGE_LENGTH + 1] = {0};
+    size_t len = MESSAGE_LENGTH;
+    while(true){
+        //Each line from stdin is a message
+        if (getline(&message, &len, stdin) < 0) {
+            printf("Failed to read line\n");
+        }
+        write_pipe(pipe_name, OP_CODE_PUBLISHER, sizeof(int) );
+        write_pipe(pipe_name, message, MESSAGE_LENGTH * sizeof(char));
+    }
+
+    // TODO: finish implementing
     printf("pub %s %s %s", register_pipe, pipe_name, box_name);
     
+
+
     return -1;
 }
