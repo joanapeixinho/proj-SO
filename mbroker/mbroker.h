944769c5412f60a3a2f1567ac096ffe4f16a688b
Peixinho <joanapeixinho0@gmail.com>
2023-01-02 22:09:39 +0000

    hey doubtful

--- mbroker/mbroker.h
+++ mbroker/mbroker.h
@@ -15,13 +15,15 @@
 
 typedef struct {
     char opcode; 
-    char client_pipename[PIPE_STRING_LENGTH + 1]; 
+
+    // TO DO: should we store the client pipe name here? or only use it to open the pipe?
+    char client_pipename[PIPE_NAME_LENGTH + 1]; 
     char box_name[BOX_NAME_LENGTH + 1]; 
     int fhandle; //file handle for the box
     size_t len; //total length of the messages in the box
     char *buffer; //buffer to store the message
-    int n_publishers = 0; //0 or 1 max
-    int n_subscribers = 0; 
+    int n_publishers; //0 or 1 max
+    int n_subscribers; 
 } box_t;
 
 /*enum client type */
@@ -42,11 +44,16 @@ typedef struct client {
     pthread_mutex_t lock;
 } client_t;
 
+// server functions
 int mbroker_init();
-void *client_session(void *client_in_array);
 void close_server(int exit_code);
+
+// client session functions
+void *client_session(void *client_in_array);
 int free_client(int session_id);
 int get_free_client();
+
+//handle functions
 int handle_tfs_register(client_t *client);
 int handle_tfs_create_box(client_t *client);
 int handle_tfs_create_box_answer(client_t *client);
@@ -56,5 +63,9 @@ int handle_tfs_list_boxes(client_t *client);
 int handle_tfs_list_boxes_answer(client_t *client);
 int handle_tfs_write(client_t *client);
 
+// parser functions
+int parser(char op_code, int parser_fnc(client_t *client));
+int parse_box(client_t *client);
+int parse_list(client_t *client);
 
 #endif
